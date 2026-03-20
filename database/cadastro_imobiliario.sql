c725ede35fded35d887c2a8f6ab811a8119cb464
Otto Machado <158228292+OttoMachado@users.noreply.github.com>
2026-03-20 02:52:01 -0300

    Adicionando banco de dados

---
+++ database/cadastro_imobiliario.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 20/03/2026 às 06:44
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cadastro_imobiliario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `imoveis`
+--
+
+CREATE TABLE `imoveis` (
+  `id` int(11) NOT NULL,
+  `logradouro` varchar(300) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `complemento` varchar(300) DEFAULT NULL,
+  `pessoa_id` int(11) NOT NULL,
+  `contribuinte_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pessoas`
+--
+
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(300) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(300) DEFAULT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `pessoas`
+--
+
+INSERT INTO `pessoas` (`id`, `nome`, `data_nascimento`, `cpf`, `sexo`, `telefone`, `email`, `usuario`, `senha`) VALUES
+(3, 'Admin', '1990-01-01', '12345678900', 'M', NULL, NULL, 'admin', '123');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `imoveis`
+--
+ALTER TABLE `imoveis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pessoa_id` (`pessoa_id`),
+  ADD KEY `fk_contribuinte` (`contribuinte_id`);
+
+--
+-- Índices de tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `imoveis`
+--
+ALTER TABLE `imoveis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `imoveis`
+--
+ALTER TABLE `imoveis`
+  ADD CONSTRAINT `fk_contribuinte` FOREIGN KEY (`contribuinte_id`) REFERENCES `pessoas` (`id`),
+  ADD CONSTRAINT `imoveis_ibfk_1` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
